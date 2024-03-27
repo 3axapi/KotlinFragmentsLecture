047a75126d28d7bebccbcb4d5e08fa57ade32771
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-03-27 13:23:29 +0100

    two lections upload

---
+++ LectionFragments/app/src/main/java/com/example/lectionfragments/ArrowFragment.kt
@@ -0,0 +1,17 @@
+package com.example.lectionfragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ArrowFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_arrow, container, false)
+    }
+}
---
+++ LectionFragments/app/src/main/java/com/example/lectionfragments/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.lectionfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var materialToolbar: MaterialToolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemsBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemsBars.left, systemsBars.top, systemsBars.right, systemsBars.bottom)
+            insets
+        }
+
+        materialToolbar = findViewById(R.id.my_toolbar)
+        setSupportActionBar(materialToolbar)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.welcome_fragment_container, WelcomeFragment())
+                .commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.arrow_forward -> replaceFragment(ArrowFragment())
+            R.id.home_page -> replaceFragment(WelcomeFragment())
+            R.id.baseline_contact -> replaceFragment(ContactFragment())
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.welcome_fragment_container, fragment)
+            .commit()
+    }
+}
